d97d2134249c76eeedda1e7687ff392b754a482f
GraysonO <gjoconnell@my.harrisburgu.edu>
2016-11-10 12:25:44 -0500

    Bubble Sort that stops after list is sorted

--- bubble.py
+++ bubble.py
@@ -1,13 +1,13 @@
 def bubbleSort(alist):
     for passnum in range(len(alist)-1,0,-1):
-        sorted = True
+        Sorted = True
         for i in range(passnum):
             if alist[i]>alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
-                sorted = False
-        if sorted == True:
+                Sorted = False
+        if Sorted == True:
             break
 alist = [54,26,93,17,77,31,44,55,20]
 bubbleSort(alist)
